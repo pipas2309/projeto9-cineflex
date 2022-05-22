7750cebae308e8e64cb0780202b547337622b497
Lucas <bazezimo42@gmail.com>
2022-05-21 22:35:53 -0300

    Version 0.05 - App() and Reset.css

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from "./GlobalStyle";
+
+
+function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Routes>
+                    
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
